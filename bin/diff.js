7585f505440953753d7b2ae079ff30aa89f6324a
Ilya Kartashou <you@example.com>
2024-06-04 20:38:09 +0300

    adding arguments and options for commander

--- bin/diff.js
+++ bin/diff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import genDiff from '../index.js';
+// import genDiff from '../index.js';
 
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --HELP', 'output usage information')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
