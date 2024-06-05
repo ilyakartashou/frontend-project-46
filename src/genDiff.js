2d98686706dff0cc3e85e53a6f2e5b9f10685a48
Ilya Kartashou <you@example.com>
2024-06-05 19:49:01 +0300

    adding of parsing function

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,3 +1,6 @@
-export default () => {
-  console.log('hello, world!');
+import parse from './parse.js';
+
+export default (filepath1, filepath2) => {
+  console.log(parse(filepath1));
+  console.log(parse(filepath2));
 };
