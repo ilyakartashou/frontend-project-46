8f8015a0544cd7ba9313f47cf0c837b7816e3c5b
Ilya Kartashou <you@example.com>
2024-06-06 21:01:06 +0300

    warning fixing

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,4 +1,4 @@
-import treeBuild from "./treeBuilder.js";
+import treeBuild from './treeBuilder.js';
 
 export default (filepath1, filepath2) => {
   treeBuild(filepath1, filepath2);
--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,5 +1,5 @@
-import parse from "./parse.js";
-import _ from "lodash";
+import _ from 'lodash';
+import parse from './parse.js';
 
 export default (filepath1, filepath2) => {
   const data1 = parse(filepath1);
@@ -9,20 +9,20 @@ export default (filepath1, filepath2) => {
   const unionKeys = _.union(keys1, keys2);
   const diffCollection = unionKeys.reduce((acc, key) => {
     if (!Object.hasOwn(data1, key)) {
-      acc.push({ key, value: data2[key], status: "+" });
+      acc.push({ key, value: data2[key], status: '+' });
     } else if (!Object.hasOwn(data2, key)) {
-      acc.push({ key, value: data1[key], status: "-" });
+      acc.push({ key, value: data1[key], status: '-' });
     } else if (data1[key] !== data2[key]) {
-      acc.push({ key, value: data1[key], status: "-" });
-      acc.push({ key, value: data2[key], status: "+" });
+      acc.push({ key, value: data1[key], status: '-' });
+      acc.push({ key, value: data2[key], status: '+' });
     } else {
-      acc.push({ key, value: data1[key], status: " " });
+      acc.push({ key, value: data1[key], status: ' ' });
     }
     return acc;
   }, []);
-  const result = _.sortBy(diffCollection, ["key"])
+  const result = _.sortBy(diffCollection, ['key'])
     .map(({ status, key, value }) => `${status} ${key}: ${value}`)
-    .join("\n");
+    .join('\n');
 
   console.log(`{\n${result}\n}`);
 };
