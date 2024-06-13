8d035b380de4c28de682b2dd1a1958e93a10462c
Ilya Kartashou <you@example.com>
2024-06-13 23:44:12 +0300

    ref

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -6,26 +6,25 @@ const buildTree = (data1, data2) => {
   const generalKeys = _.union(keys1, keys2);
   const result = generalKeys.reduce((acc, key) => {
     if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-      acc.push({
+     return [...acc,{
         key,
         value: buildTree(data1[key], data2[key]),
         status: 'nested',
-      });
+      }];
     } else if (!Object.hasOwn(data1, key)) {
-      acc.push({ key, value: data2[key], status: 'added' });
+     return [...acc,{ key, value: data2[key], status: 'added' }];
     } else if (!Object.hasOwn(data2, key)) {
-      acc.push({ key, value: data1[key], status: 'deleted' });
+      return [...acc,{ key, value: data1[key], status: 'deleted' }];
     } else if (data1[key] !== data2[key]) {
-      acc.push({
+     return [...acc,{
         key,
         oldValue: data1[key],
         newValue: data2[key],
         status: 'changed',
-      });
+      }];
     } else {
-      acc.push({ key, value: data1[key], status: 'unchanged' });
+      return [...acc,{ key, value: data1[key], status: 'unchanged' }];
     }
-    return acc;
   }, []);
   const temp = _.sortBy(result, ['key']);
   return temp;
