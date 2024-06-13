9993ffe74802ee01ab4aaa2bb9ea775cd2e0ad5e
Ilya Kartashou <you@example.com>
2024-06-13 22:38:38 +0300

    adding asciinema

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -1,34 +1,34 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 const getOutput = (value) => {
   if (_.isObject(value)) {
-    return "[complex value]";
+    return '[complex value]';
   }
-  if (typeof value === "string") {
+  if (typeof value === 'string') {
     return `'${value}'`;
   }
   return value;
 };
 const plain = (tree) => {
-  const iter = (node, start = "") => {
+  const iter = (node, start = '') => {
     const result = node
-      .filter((el) => el.status !== "unchanged")
+      .filter((el) => el.status !== 'unchanged')
       .map((el) => {
         const first = `${start}${el.key}`;
         switch (el.status) {
-          case "added":
+          case 'added':
             return `    Property '${first}' was added with value: ${getOutput(el.value)}`;
-          case "deleted":
+          case 'deleted':
             return `    Property '${first}' was removed`;
-          case "changed":
+          case 'changed':
             return `    Property '${first}' was updated. From ${getOutput(el.oldValue)} to ${getOutput(el.newValue)}`;
-          case "nested":
+          case 'nested':
             return iter(el.value, `${first}.`);
           default:
             throw new Error(`Invalid status - ${el.status}`);
         }
       });
-    return result.join("\n");
+    return result.join('\n');
   };
 
   return iter(tree);
