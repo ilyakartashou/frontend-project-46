cd72c86b1775e2f4eb4bc2f51747f60b43553459
Ilya Kartashou <you@example.com>
2024-06-13 23:45:21 +0300

    ref

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -6,25 +6,24 @@ const buildTree = (data1, data2) => {
   const generalKeys = _.union(keys1, keys2);
   const result = generalKeys.reduce((acc, key) => {
     if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-     return [...acc,{
+      return [...acc, {
         key,
         value: buildTree(data1[key], data2[key]),
         status: 'nested',
       }];
-    } else if (!Object.hasOwn(data1, key)) {
-     return [...acc,{ key, value: data2[key], status: 'added' }];
-    } else if (!Object.hasOwn(data2, key)) {
-      return [...acc,{ key, value: data1[key], status: 'deleted' }];
-    } else if (data1[key] !== data2[key]) {
-     return [...acc,{
+    } if (!Object.hasOwn(data1, key)) {
+      return [...acc, { key, value: data2[key], status: 'added' }];
+    } if (!Object.hasOwn(data2, key)) {
+      return [...acc, { key, value: data1[key], status: 'deleted' }];
+    } if (data1[key] !== data2[key]) {
+      return [...acc, {
         key,
         oldValue: data1[key],
         newValue: data2[key],
         status: 'changed',
       }];
-    } else {
-      return [...acc,{ key, value: data1[key], status: 'unchanged' }];
     }
+    return [...acc, { key, value: data1[key], status: 'unchanged' }];
   }, []);
   const temp = _.sortBy(result, ['key']);
   return temp;
